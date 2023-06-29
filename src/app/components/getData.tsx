6d1c1b5f6ef4d249c06cf0f5044c2a5c3f42f40f
b13d <4.topppp@mail.ru>
2023-06-29 13:03:09 +0400

    add styles, and fix logic

--- src/app/components/getData.tsx
+++ src/app/components/getData.tsx
@@ -18,53 +18,47 @@ export default async function getData() {
     "https://hacker-news.firebaseio.com/v0/newstories.json?print=pretty"
   );
 
-  // console.log(res.data);
-
-  let q = res.headers["Content-Length"];
-
   let arr: string[] = [];
-  // console.log(arrNews);
 
   res.data.map((value: string) => {
-    // console.log(value);
     arr.push(value);
   });
 
-  let resArr: string[] = arr.sort().slice(400, 500);
+  let resArr: string[] = arr.sort().slice(400, 500).reverse();
 
   let itemsArr: IItems[] = [];
 
-  resArr.map(async (value, index) => {
-    console.log("xxx");
+  // console.log("he");
+
+  // console.log(itemsArr);
+
+  return resArr.map(async (value, index) => {
+    // console.log("xxx");
 
     let tempAxios = await axios.get<IItems>(
       `https://hacker-news.firebaseio.com/v0/item/${value}.json?print=pretty`
     );
 
-    itemsArr.push(tempAxios.data);
-  });
-
-  console.log();
-
-  console.log("he");
-
-  console.log(itemsArr);
-
-  return itemsArr.map((value) => {
+    console.log(tempAxios.data.kids);
     return (
-      <div key={value.id}>
-        <h1>id: {value.id}</h1>
-        <h1>title: {value.title}</h1>
+      <div
+        className="px-10 py-4 gap-5 flex flex-col rounded-tl-3xl rounded-br-3xl rounded-md bg-white"
+        key={tempAxios.data.id}
+      >
+        <div>
+          <h1>id: {tempAxios.data.id}</h1>
+          <p className="w-[500px]">title: {tempAxios.data.title}</p>
+          <p>score: {tempAxios.data.score}</p>
+        </div>
+        <div className="flex gap-1">
+          <img className="w-[25px]" src="/images/chat.png" alt="chat-icon" />
+          {tempAxios.data.kids !== undefined ? (
+            <h2>{tempAxios.data.kids.length} comments</h2>
+          ) : (
+            <h2>0 comments</h2>
+          )}
+        </div>
       </div>
     );
   });
-
-  // arr.map((value, index) => {
-  //   console.log(value);
-  //   idArr.push(value);
-  //   return <p key={index}>{value}</p>;
-  // });
-
-  // console.log(idArr.length);
-  // return res.json();
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import GetData from "./components/getData";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 w-[1000px] gap-4 m-auto">
       <GetData />
     </main>
   );
