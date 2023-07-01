4210544fbfe0e50fa904bd452780ee05158f281f
b13d <4.topppp@mail.ru>
2023-07-01 19:48:18 +0400

    add icon site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,6 +16,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <link
+          rel="icon"
+          href="/icon.png"
+          type="image/png"
+          sizes="32x32"
+        />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
